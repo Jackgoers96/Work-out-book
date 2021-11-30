0fe399afd7ee98f0275bd6148ef925c6f261cdef
Jack Goers <jackgoers@gmail.com>
2021-11-30 17:31:42 -0600

    Transfer

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -5,60 +5,58 @@ router.get("/", (req, res) => {
   db.Workout.aggregate([
     {
       $addFields: {
-        totalDuration: { $sum: "$exercises.duration" },
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
       },
     },
   ])
-    .then((getWorkout) => {
-      res.json(getWorkout);
+    .then((data) => {
+      res.json(data);
     })
     .catch((err) => {
       res.json(err);
     });
 });
 
-router.post('/', async (req, res) => {
-    try{
-        const newWorkout = await db.Workout.create({})
-        res.json(newWorkout);
-    }
-    catch (err){
-        console.log(err);
-    }
-});
-
-
-router.put('/:id', async ({ body, params }, res) => {
-    try{
-       await db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body }})
-       .then((newWorkout) => {
-        res.json(newWorkout);
-    })
-    }
-    catch (err){
-        console.log(err);
-    }
-});
-
-
-router.get("/range", async (req, res) => {
-  try {
-    const allWorkouts = await db.Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: { $sum: "$exercises.duration" },
+router.get("/range", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
         },
       },
-    ])
-      .limit(7)
-      .then((allWorkouts) => {
-        res.json(allWorkouts);
-      });
-  } catch (err) {
-    console.log(err);
-  }
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
+router.post("/", (req, res) => {
+  db.Workout.create({})
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log("Error:", err);
+      res.json(err);
+    });
+});
 
+router.put("/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log("Error:", err);
+      res.json(err);
+    });
+});
 
 module.exports = router;
